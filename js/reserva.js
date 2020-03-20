0f184734a997d65c2f573a3ad036f76358686611
patrikmolina <52463966+patrikmolina@users.noreply.github.com>
2020-03-20 20:53:15 -0300

    Add files via upload
    Reservando Final

--- js/reserva.js
+++ js/reserva.js
@@ -1,42 +1,62 @@
-var Reserva = function (Horario, CantidadDePersonas, PrecioPorPersona, CodigoDeDescuento){
+var Reserva = function (horario, cantidadPersonas, precioPorPersona, codigoDeDescuento) {
     this.horario = horario;
     this.cantidadPersonas = cantidadPersonas;
-    this.PrecioPorPersona = PrecioPorPersona;
-    this.CodDescuento = CodigoDeDescuento;
+    this.precioPorPersona = precioPorPersona;
+    this.codigoDeDescuento = codigoDeDescuento;
+}
+Reserva.prototype.precioBase = function () {
+    let precioBase = this.cantidadPersonas * this.precioPorPersona
+    if (precioBase < 0) {
+        return ('Ha ingresado un número incorrecto')
+    }
+    return precioBase;
 }
 
-Reserva.prototype.precioDeReserva = function (cantidadDePersonas, precioPersona) {
-    let precioReserva = cantidadDePersonas*precioPersona;
-    return precioReserva;
-}
-
-Reserva.prototype.precioFinalReserva = function(precioBase, adicional, descuentos){
-    let precioFinal = this.preciobase(cantidadDePersonas,precioPersona) + adicional - descuentos
+Reserva.prototype.precioFinalReserva = function (adicional, descuentos) {
+    debugger
+    let precioBase = this.precioBase();
+    let precioFinal = precioBase -  this.descuentoPorCodigo(precioBase) + this.adicional(precioBase) - this.descuentosPorGruposGrandes(precioBase);
     return precioFinal;
 }
 
-Reserva.prototype.descuentosPorGruposGrandes = function (){
-    switch (cantidadPersonas) {
-        case 1:
-            if (this.cantidadPersonas >=4 || this.cantidadPersonas <= 6){
-                return total*0,05;
-            }
-            break
-        case 2: 
-        if (this.cantidadPersonas >=7 || this.cantidadPersonas <= 8){
-            return total*0,10;
-        }
-            break;
-            case 3: 
-        if (this.cantidadPersonas >9){
-            return total*0,15;
-        }
-            break;
-        default:
-            break;
+Reserva.prototype.descuentosPorGruposGrandes = function (precioBase) {
+
+    if (this.cantidadPersonas >= 4 || this.cantidadPersonas <= 6) {
+        return precioBase * 0.05;
+    }
+
+    if (this.cantidadPersonas >= 7 || this.cantidadPersonas <= 8) {
+        return precioBase * 0.1;
+    }
+
+    if (this.cantidadPersonas >= 9) {
+        return precioBase * 0.15;
     }
 }
 
-Reserva.prototype.descuentoPorCodigo = function(){
-    
+
+Reserva.prototype.descuentoPorCodigo = function (precioBase) {
+    if (this.codigoDeDescuento == 'DES15') {
+        return   precioBase * 0.15;
+    };
+
+    if (this.codigoDeDescuento == 'DES200') {
+        return 200;
+    };
+
+    if (this.codigoDeDescuento == 'DES1') {
+        return  this.precioPorPersona;
+    }
+}
+
+Reserva.prototype.adicional = function (precioBase) {
+    if ((this.horario.getHours() >= 13 && this.horario.getHours() <= 14) || (this.horario.getHours() >= 20 && this.horario.getHours() <= 21)) {
+         
+        return precioBase * 0.05;
+    }
+  
+  let diaDeAdicional = this.horario.getDay();
+    if (diaDeAdicional == 0 || diaDeAdicional > 4   ){
+        return precioBase * 0.1;
+    }
 }
--- js/tests.js
+++ js/tests.js
@@ -1,73 +1,73 @@
 var expect = chai.expect;
 
-function mockRestaurant(){
+function mockRestaurant() {
     return new Restaurant(1, "TAO Uptown", "Asiática", "Nueva York", ["13:00", "15:30", "18:00"], "../img/asiatica1.jpg", [6, 7, 9, 10, 5]);
 };
 
-function mockListadoRestaurant(){
-    var listadoDeRestaurantes= [
-    new Restaurant(2, "Mandarín Kitchen", "Asiática", "Londres", ["15:00", "14:30", "12:30"], "../img/asiatica2.jpg", [7, 7, 3, 9, 7]),
-    new Restaurant(3, "Burgermeister", "Hamburguesa", "Berlín", ["11:30", "12:00", "22:30"], "../img/hamburguesa4.jpg", [5, 8, 4, 9, 9]),
-    new Restaurant(4, "Bleecker Street Pizza", "Pizza", "Nueva York", ["12:00", "15:00", "17:30"], "../img/pizza2.jpg", [8, 9, 9, 4, 6, 7]),
-    new Restaurant(5, "Jolly", "Asiática", "Berlín", ["12:00", "13:30", "16:00"], "../img/asiatica3.jpg", [8, 3, 9, 5, 6, 7]),
-    new Restaurant(6, "Green salad", "Ensalada", "Berlín", ["17:00", "19:00", "20:30"], "../img/ensalada2.jpg", [8, 3, 2, 1, 8, 7]),
-    new Restaurant(7, "Osteria Da Fortunata", "Pasta", "Roma", ["13:00", "15:30", "18:00"], "../img/pasta2.jpg", [7, 7, 7, 7, 3, 9]),
-    new Restaurant(8, "Cafe Francoeur", "Desayuno", "París", ["14:30", "15:30", "19:00"], "../img/desayuno1.jpg", [4, 7, 9, 8, 10]),
-    new Restaurant(9, "La Trottinette", "Pasta", "París", ["16:00", "18:00", "21:30"], "../img/pasta5.jpg", [8, 8, 7, 7, 7, 7]),
-    new Restaurant(10, "New London Cafe", "Desayuno", "Londres", ["12:00", "13:00", "14:30"], "../img/desayuno3.jpg", [9, 4, 6, 5, 6]),
-    new Restaurant(11, "Frogburguer", "Hamburguesa", "París", ["12:00", "15:00", "17:30"], "../img/hamburguesa1.jpg", [9, 8, 5, 2, 9]),]
-            return new Listado(listadoDeRestaurantes);
+function mockListadoRestaurant() {
+    var listadoDeRestaurantes = [
+        new Restaurant(2, "Mandarín Kitchen", "Asiática", "Londres", ["15:00", "14:30", "12:30"], "../img/asiatica2.jpg", [7, 7, 3, 9, 7]),
+        new Restaurant(3, "Burgermeister", "Hamburguesa", "Berlín", ["11:30", "12:00", "22:30"], "../img/hamburguesa4.jpg", [5, 8, 4, 9, 9]),
+        new Restaurant(4, "Bleecker Street Pizza", "Pizza", "Nueva York", ["12:00", "15:00", "17:30"], "../img/pizza2.jpg", [8, 9, 9, 4, 6, 7]),
+        new Restaurant(5, "Jolly", "Asiática", "Berlín", ["12:00", "13:30", "16:00"], "../img/asiatica3.jpg", [8, 3, 9, 5, 6, 7]),
+        new Restaurant(6, "Green salad", "Ensalada", "Berlín", ["17:00", "19:00", "20:30"], "../img/ensalada2.jpg", [8, 3, 2, 1, 8, 7]),
+        new Restaurant(7, "Osteria Da Fortunata", "Pasta", "Roma", ["13:00", "15:30", "18:00"], "../img/pasta2.jpg", [7, 7, 7, 7, 3, 9]),
+        new Restaurant(8, "Cafe Francoeur", "Desayuno", "París", ["14:30", "15:30", "19:00"], "../img/desayuno1.jpg", [4, 7, 9, 8, 10]),
+        new Restaurant(9, "La Trottinette", "Pasta", "París", ["16:00", "18:00", "21:30"], "../img/pasta5.jpg", [8, 8, 7, 7, 7, 7]),
+        new Restaurant(10, "New London Cafe", "Desayuno", "Londres", ["12:00", "13:00", "14:30"], "../img/desayuno3.jpg", [9, 4, 6, 5, 6]),
+        new Restaurant(11, "Frogburguer", "Hamburguesa", "París", ["12:00", "15:00", "17:30"], "../img/hamburguesa1.jpg", [9, 8, 5, 2, 9]),]
+    return new Listado(listadoDeRestaurantes);
 };
 
-describe('test Reserva Horarios', ()=> {
-    
+describe('Test Reserva Horarios', () => {
+
     var restaurante;
     var horariosOriginal;
     var horariosControl;
 
-    beforeEach(function(){
+    beforeEach(function () {
         restaurante = mockRestaurant();
         horariosOriginal = restaurante.horarios;
         horariosControl = [...restaurante.horarios];
     })
 
-    it('Si esta disponible el horario, se elimina', function(){
+    it('Si esta disponible el horario, se elimina', function () {
         //["13:00", "15:30", "18:00"]
         let horarioAReservar = horariosOriginal[1];
         let resultado = restaurante.reservarHorario(horarioAReservar);
         expect(resultado).that.does.not.include(horarioAReservar);
     });
 
-    it('Si el horario que se reserva no existe, el array se mantiene igual', function(){
+    it('Si el horario que se reserva no existe, el array se mantiene igual', function () {
         let horarioAReservar = "20:30";
         restaurante.reservarHorario(horarioAReservar);
         expect(horariosOriginal).to.eql(horariosControl);
     });
 
-    it('Intenta reservar un horario sin pasar ningún parametro en la función, el array se mantiene igual',()=>{
+    it('Intenta reservar un horario sin pasar ningún parametro en la función, el array se mantiene igual', () => {
         restaurante.reservarHorario();
         expect(horariosOriginal).to.eql(horariosControl);
     });
 });
 
-describe('test Puntuacion', ()=>{
+describe('Test Puntuacion', () => {
 
     let restaurante;
     let calificaciones;
- 
-    beforeEach(function() {
+
+    beforeEach(function () {
         //[6, 7, 9, 10, 5]
         restaurante = mockRestaurant();
-        calificaciones  = restaurante.calificaciones;
+        calificaciones = restaurante.calificaciones;
     });
 
-    it('Dado un restaurant con X calificaciones la puntuación (promedio de las mismas) se calcula de forma correcta',function(){
+    it('Dado un restaurant con X calificaciones la puntuación (promedio de las mismas) se calcula de forma correcta', function () {
         let promedioEsperado = 7.4;
         let promedioObtenido = restaurante.obtenerPuntuacion();
         expect(promedioObtenido).to.be.equal(promedioEsperado);
     });
 
-    it ('Dado un array vacio, devuelve el valor 0', function(){
+    it('Dado un array vacio, devuelve el valor 0', function () {
         restaurante.calificaciones = [];
         let promedioObtenido = restaurante.obtenerPuntuacion();
         expect(promedioObtenido).to.be.equal(0);
@@ -75,76 +75,76 @@ describe('test Puntuacion', ()=>{
 
 });
 
-describe ('Test de Calificaciones', () => {
+describe('Test de Calificaciones', () => {
     /*  
         1-Agregar la calificación (8) y chequear si se agrego
         2-Agregar un valor negativo y corrobor que no se agruegue
         3-Que no agregue numeros con decimales
         4-Agregar un string y que no se refleje  */
 
-        let restaurante;
+    let restaurante;
 
-        beforeEach(function(){
-            restaurante = mockRestaurant();
-        });
+    beforeEach(function () {
+        restaurante = mockRestaurant();
+    });
 
-        it('Agregar una calificacion (8) y corroborar si se agrega en el array de calificaciones', function(){
-            let arrayCalificaciones = restaurante.calificaciones
-            restaurante.calificar(8);
-            expect(arrayCalificaciones).that.does.include(8);
-        });
+    it('Agregar una calificacion (8) y corroborar si se agrega en el array de calificaciones', function () {
+        let arrayCalificaciones = restaurante.calificaciones
+        restaurante.calificar(8);
+        expect(arrayCalificaciones).that.does.include(8);
+    });
 
-        it('Agregar un valor negativo y el array de calificaciones se mantiene igual', function(){
-            let arrayCalificaciones = restaurante.calificaciones
-            restaurante.calificar(-8);
-            expect(arrayCalificaciones).that.not.include(-8);
-        });
+    it('Agregar un valor negativo y el array de calificaciones se mantiene igual', function () {
+        let arrayCalificaciones = restaurante.calificaciones
+        restaurante.calificar(-8);
+        expect(arrayCalificaciones).that.not.include(-8);
+    });
 
-        it('Agregar un numero decimal y que el array se mantenga igual', function(){
-            let arrayCalificaciones = restaurante.calificaciones
-            restaurante.calificar(8.29);
-            expect(arrayCalificaciones).that.not.include(8.29);
-        });
+    it('Agregar un numero decimal y que el array se mantenga igual', function () {
+        let arrayCalificaciones = restaurante.calificaciones
+        restaurante.calificar(8.29);
+        expect(arrayCalificaciones).that.not.include(8.29);
+    });
 
-        it('gregar un string y que no se refleje', function(){
-            let arrayCalificaciones = restaurante.calificaciones
-            restaurante.calificar('hola');
-            expect(arrayCalificaciones).that.not.include('hola');
-        });
+    it('gregar un string y que no se refleje', function () {
+        let arrayCalificaciones = restaurante.calificaciones
+        restaurante.calificar('hola');
+        expect(arrayCalificaciones).that.not.include('hola');
+    });
 
 });
 
-describe('Test buscar restaurante', () =>{
-    
+describe('Test buscar restaurante', () => {
+
     /*
         1-Buscar un ID existente y que nos devuelva el restaurante correcto
         2-Buscar un ID inexistente y que nos devuelva "No se ha encontrado ningún restaurant"   
         3-Buscar un String y nos devuelva "No se ha encontrado ningún restaurant"
     */
-        
-    beforeEach(function(){
-        restaurante = mockListadoRestaurant();
+
+    beforeEach(function () {
+       // restaurante = mockListadoRestaurant();
     });
-        it ('Buscar un ID existente y que nos devuelva el restaurante correcto', function(){
-            
-            var IdRestaurant = listado.buscarRestaurante(1);
-                expect(IdRestaurant.nombre).to.be.equal(listadoDeRestaurantes[0].nombre);
-            });
+    it('Buscar un ID existente y que nos devuelva el restaurante correcto', function () {
 
-        it('Buscar un ID inexistente y que nos devuelva "No se ha encontrado ningún restaurant', () =>{
+        var IdRestaurant = listado.buscarRestaurante(1);
+        expect(IdRestaurant.nombre).to.be.equal(listadoDeRestaurantes[0].nombre);
+    });
 
-            let IdRestaurant = listado.buscarRestaurante(39)
-            expect(IdRestaurant).to.be.equal("No se ha encontrado ningún restaurant");
-        });
+    it('Buscar un ID inexistente y que nos devuelva "No se ha encontrado ningún restaurant', () => {
 
-        it('Buscar un String y nos devuelva "No se ha encontrado ningún restaurant"', () =>{
-            let IdRestaurant = listado.buscarRestaurante("olavarria")
-            expect(IdRestaurant).to.be.equal("No se ha encontrado ningún restaurant");
-        })
+        let IdRestaurant = listado.buscarRestaurante(39)
+        expect(IdRestaurant).to.be.equal("No se ha encontrado ningún restaurant");
+    });
+
+    it('Buscar un String y nos devuelva "No se ha encontrado ningún restaurant"', () => {
+        let IdRestaurant = listado.buscarRestaurante("olavarria")
+        expect(IdRestaurant).to.be.equal("No se ha encontrado ningún restaurant");
+    })
 });
 
 
-describe(' Test Obtener Restaurante', () =>{
+describe(' Test Obtener Restaurante', () => {
 
     /*
     1-Buscar un filtroRubro, filtroCiudad y filtroHorario y que te retorne un valor valido
@@ -153,23 +153,80 @@ describe(' Test Obtener Restaurante', () =>{
     4-Buscar un filtroRubro invalido y filtroCiudad  y filtroHorario valido y que retorne "No se encontraron resultados" 
     */
 
-        it ('Buscar un filtroRubro, filtroCiudad y filtroHorario y que te retorne un valor valido', () =>{
-         let filtroRestaurante = listado.obtenerRestaurantes("Asiática", "Londres", ["15:00", "14:30", "12:30"]);
-         expect(filtroRestaurante[0].nombre).to.be.equal("Mandarín Kitchen");
-        })
+    it('Buscar un filtroRubro, filtroCiudad y filtroHorario y que te retorne un valor valido', () => {
+        let filtroRestaurante = listado.obtenerRestaurantes("Asiática", "Londres", ["15:00", "14:30", "12:30"]);
+        expect(filtroRestaurante[0].nombre).to.be.equal("Mandarín Kitchen");
+    })
 
-        it ('Buscar un filtroRubro y filtroCiudad valido y filtroHorario invalido y que retorne "No se encontraron resultados"', () =>{
-            let filtroRestaurante = listado.obtenerRestaurantes("Asiatica", "Londres", ["23:00", "15:00", "38:00"]);
-            expect(filtroRestaurante).to.be.equal("No se encontraron resultados");
-        })
+    it('Buscar un filtroRubro y filtroCiudad valido y filtroHorario invalido y que retorne "No se encontraron resultados"', () => {
+        let filtroRestaurante = listado.obtenerRestaurantes("Asiatica", "Londres", ["23:00", "15:00", "38:00"]);
+        expect(filtroRestaurante).to.be.equal("No se encontraron resultados");
+    })
 
-        it ('Buscar un filtroRubro y filtroHorario valido y filtroCiudad invalido y que retorne "No se encontraron resultados"', () =>{
-         let filtroRestaurante = listado.obtenerRestaurantes("Asiatica", "Nueva York", ["15:00", "14:30", "12:30"]);
-         expect(filtroRestaurante).to.be.equal("No se encontraron resultados");
-        })
+    it('Buscar un filtroRubro y filtroHorario valido y filtroCiudad invalido y que retorne "No se encontraron resultados"', () => {
+        let filtroRestaurante = listado.obtenerRestaurantes("Asiatica", "Nueva York", ["15:00", "14:30", "12:30"]);
+        expect(filtroRestaurante).to.be.equal("No se encontraron resultados");
+    })
 
-        it ('Buscar un filtroRubro invalido y filtroCiudad  y filtroHorario valido y que retorne "No se encontraron resultados"', () =>{
-            let filtroRestaurante = listado.obtenerRestaurantes("Hamburguesa", "Londres", ["15:00", "14:30", "12:30"]);
-            expect(filtroRestaurante).to.be.equal("No se encontraron resultados");
-        })
+    it('Buscar un filtroRubro invalido y filtroCiudad  y filtroHorario valido y que retorne "No se encontraron resultados"', () => {
+        let filtroRestaurante = listado.obtenerRestaurantes("Hamburguesa", "Londres", ["15:00", "14:30", "12:30"]);
+        expect(filtroRestaurante).to.be.equal("No se encontraron resultados");
+    })
 });
+
+
+
+
+describe('Test Precio Base', () => {
+    /*
+    1- Ingresar un numero de persona y un precio por persona valido y que de un precio base de resultado
+    2- Ingresar un numero de persona negativo y un precio valido y que de mensaje de error
+    3- Ingresar un numero de persona valido y un precio negativo y que de un mensaje de error
+    */
+
+    it('Ingresar un numero de persona y un precio por persona valido y que de un precio base de resultado', () => {
+        let miFecha = new Date()
+        let reserva = new Reserva (miFecha, 2, 1000, 0)
+        let precioBase = reserva.precioBase()
+        expect(precioBase).to.be.equal(2000)
+    })
+
+    it('Ingresar un numero de persona negativo y un precio valido y que de mensaje de error', () => {
+        let miFecha = new Date()
+        let reserva = new Reserva (miFecha, -2, 1000, 0)
+        let precioBase = reserva.precioBase()
+        expect(precioBase).to.be.equal('Ha ingresado un número incorrecto');
+    })
+
+    it('Ingresar un numero de persona valido y un precio negativo y que de un mensaje de error', () => {
+        let miFecha = new Date()
+        let reserva = new Reserva (miFecha, 2, -1000, 0)
+        let precioBase = reserva.precioBase()
+        expect(precioBase).to.be.equal('Ha ingresado un número incorrecto');
+    })
+})
+
+describe('Test Precio Total de una reserva', ()=>{
+    /*
+    1- Ingresar una reserva de 4 personas y verificar que se aplique un 5% de descuento
+    2- Ingresar una reserva de 8 personas y verificar que se aplique un 10% de descuento
+    3- Ingresar una reserva de 10 personas y verificar que se aplique un 15% de descuento
+    4- ingresar el codigo DES200 y verificar que se aplique $200 de descuento
+    5- Ingresar una reserva de 5 personas a las 13.30 y verificar que se aplique un adicional de 5%
+    6- Ingresar una reserva un día sábad y verificar que se aplique un adicional de 10%
+    */
+   it('Ingresar una reserva de 4 personas y verificar que se aplique un 5% de descuento',() =>{
+    var reserva1 = new Reserva (new Date(2018, 7, 24, 11, 00), 8, 350, "DES1")
+    var reserva2 = new Reserva (new Date(2018, 7, 27, 14, 10), 2, 150, "DES200")
+    
+    let precioBaseReserva1 = reserva1.precioBase(); 
+    let precioFinalReserva1 = reserva1.precioFinalReserva();
+    expect(precioBaseReserva1).to.be.equal(2800);
+    expect(precioFinalReserva1).to.be.equal(2590);
+    
+    let precioBaseReserva2 = reserva2.precioBase();
+    let precioFinalReserva2 = reserva2.precioFinalReserva();
+    expect(precioBaseReserva2).to.be.equal(300);
+    expect(precioFinalReserva2).to.be.equal(100);
+   })
+})
